ff83baa3cb07ed030e73dc1b17e0f0807b449019
Milan Antonovic <milan.antonovic@gmail.com>
2013-11-12 13:42:49 +0000

    acquisition scripts test

--- test/scripts/testImporter.py
+++ test/scripts/testImporter.py
@@ -16,8 +16,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #---------------------------------------------------------------------------
-# Created on Mon Nov 11 11:05:41 2013
-#---------------------------------------------------------------------------
 """
 description:
     
@@ -54,45 +52,54 @@ class CampbellImporter(importer.Importer):
             s = int(second)
         return (datetime(y, 1, 1, h, m, s, 0) + timedelta(days=(d-dDiff)))
     
-    def customParse(self, fileObj, name, column, observation):
-        
+    def customParse(self, fileObj, name, column, conf, dateConf=[1,2,3]):
         for line in fileObj.readlines():
             arr = line.strip(' \t\n\r').split(",")            
             if len(arr)>0:
-                date = self.getDate(arr[1],arr[2],arr[3])
+                
+                date = self.getDate(arr[dateConf[0]],arr[dateConf[1]],arr[dateConf[2]])
                 self.setEndPosition(date)
+                
                 if arr[0] == column:
+                    
+                    if len(dateConf)==3:
+                        date = self.getDate(arr[dateConf[0]],arr[dateConf[1]],arr[dateConf[2]])
+                    elif len(dateConf)==4:
+                        date = self.getDate(arr[dateConf[0]],arr[dateConf[1]],arr[dateConf[2]],arr[dateConf[3]])
+                    else:
+                        raise importer.IstSOSError("Date configuration mismatch.")
+                    self.setEndPosition(date)
+                    
                     values = {}
-                    for obs in observation.keys():
-                        values[obs] = arr[observation[obs]]
+                    for obs in conf.keys():
+                        if type(conf[obs])==type(""):
+                            values[obs] = float(conf[obs])
+                        else:
+                            values[obs] = arr[conf[obs]]
                     self.addObservation(
                         importer.Observation(date, values)
                     )
     
 class TLuganoImporter(CampbellImporter):
-    
     def __init__(self):
         importer.Importer.__init__(self, 'T_LUGANO', 
             'http://localhost/istsos', 'demo',
-            path.normpath("%s/test/scripts/data" % path.abspath(".")), 'T_LUGANO.dat', 
+            path.normpath("%s/test/scripts/data" % path.abspath(".")), 'LUGANO.dat', 
             path.normpath("%s/test/scripts/data" % path.abspath(".")),
             debug=True)
-    
     def parse(self, fileObj, name):
         self.customParse(fileObj, name, '101', {
             "urn:ogc:def:parameter:x-istsos:1.0:meteo:air:temperature": 6
         })
         
 class PLuganoImporter(CampbellImporter):
-    
     def __init__(self):
-        importer.Importer.__init__(self, 'T_LUGANO', 
+        importer.Importer.__init__(self, 'P_LUGANO', 
             'http://localhost/istsos', 'demo',
-            path.normpath("%s/test/scripts/data" % path.abspath(".")), 'T_LUGANO.dat', 
+            path.normpath("%s/test/scripts/data" % path.abspath(".")), 'LUGANO.dat', 
             path.normpath("%s/test/scripts/data" % path.abspath(".")),
             debug=True)
-    
     def parse(self, fileObj, name):
-        self.customParse(fileObj, name, '101', {
-            "urn:ogc:def:parameter:x-istsos:1.0:meteo:air:temperature": 6
-        })
+        self.customParse(fileObj, name, '99', {
+            "urn:ogc:def:parameter:x-istsos:1.0:meteo:air:rainfall": "0.2"
+        }, [1,2,3,4])
