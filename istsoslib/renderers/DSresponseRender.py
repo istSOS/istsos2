d9f147d23d046a515e76a49a2a6c4c9d35af2038
Massimiliano Cannata <massimiliano.cannata@gmail.com>
2013-12-20 13:58:17 +0000

    istSOS OGC test: declaration xml

--- istsoslib/renderers/DSresponseRender.py
+++ istsoslib/renderers/DSresponseRender.py
@@ -183,4 +183,4 @@ def render(DS,sosConfig):
                 raise Exception("Constraint definition invalid in the database for %s" % field["def_opr"])
         
     root = tree.getroot()
-    return et.tostring(root)
+    return """<?xml version="1.0" encoding="UTF-8"?>\n%s""" % et.tostring(root)
