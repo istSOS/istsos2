4f8a4b93824d6f0bbe23a373c806f150d20c7336
Milan Antonovic <milan.antonovic@gmail.com>
2015-07-10 14:16:00 +0000

    cleaning old doc files

--- site/tutorial/dataset/runimport.sh
+++
@@ -1,4 +0,0 @@
-python cmdimportcsv.py -s demo -u http://192.168.56.101/istsos -p T_LUGANO -w /home/milan/Desktop/ISTSOS-WS/dati
-python cmdimportcsv.py -s demo -u http://192.168.56.101/istsos -p P_LUGANO -w /home/milan/Desktop/ISTSOS-WS/dati
-python cmdimportcsv.py -s demo -u http://192.168.56.101/istsos -p LOCARNO -w /home/milan/Desktop/ISTSOS-WS/dati
-python cmdimportcsv.py -s demo -u http://192.168.56.101/istsos -p BELLINZONA -w /home/milan/Desktop/ISTSOS-WS/dati
