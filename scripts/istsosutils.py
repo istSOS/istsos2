64e0988caaa46178679d1c832b8ecb7622f4125b
Antonovic P. Milan <milan.antonovic@gmail.com>
2021-11-25 17:29:11 +0100

    embedding parse_and_get in scripts folder

--- scripts/istsosutils.py
+++ scripts/istsosutils.py
@@ -25,8 +25,7 @@ import sys
 from datetime import timedelta
 from datetime import datetime
 import pprint
-from io import StringIO
-from parse_and_get import parse_and_get_ns
+from io import StringIO, BytesIO
 import json
 try:
     import requests as req
@@ -108,7 +107,7 @@ class Service(object):
             self.host, self.service), params=params, auth=self.auth)
 
         # Parsing response
-        gc, gcNs = self.parse_and_get_ns(res.content)
+        gc, gcNs = parse_and_get_ns(res.content)
 
         offerings = gc.findall(
             "{%s}Contents/{%s}ObservationOfferingList/"
@@ -536,3 +535,39 @@ class Procedure(dict):
                 "constraint": constraint
             })
         return request
+
+
+def parse_and_get_ns(source):
+    events = "start", "start-ns"
+    root = None
+    ns = {}
+    
+    if isinstance(source, bytes):
+        if not isinstance(source, BytesIO) and path.isfile(source):
+            file_parsed = open(source, 'rb')
+        elif not hasattr(source, 'read'):
+            file_parsed = BytesIO(source)
+        else:
+            file_parsed = source
+            
+    else:
+        if not isinstance(source, StringIO) and path.isfile(source):
+            file_parsed = open(source, 'rb')
+        elif not hasattr(source, 'read'):
+            file_parsed = StringIO(source)
+        else:
+            file_parsed = source
+
+    for event, elem in et.iterparse(file_parsed, events):
+        if event == "start-ns":
+            if elem[0] in ns and ns[elem[0]] != elem[1]:
+                # NOTE: It is perfectly valid to have the same prefix refer
+                #   to different URI namespaces in different parts of the
+                #   document. This exception serves as a reminder that this
+                #   solution is not robust.  Use at your own peril.
+                raise KeyError("Duplicate prefix with different URI found.")
+            ns[elem[0]] = "%s" % elem[1]
+        elif event == "start":
+            if root is None:
+                root = elem
+    return et.ElementTree(root), ns
